3262035d913421e084b70a188fa9ad3b9e0e5c0a
DRam76 <96955718+DRam76@users.noreply.github.com>
2026-05-08 22:46:32 +0100

    Update backend.tf

--- 07may2026-repos-terraform/infra/backend.tf
+++ 07may2026-repos-terraform/infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "teraftfstateprod001"
-    container_name       = "tfstate"
+    container_name       = "tfstate-dev"
 #    key                  = "infra-${var.environment}.tfstate"
     key                  = "infra-dev.tfstate"
     use_azuread_auth = true   # ← skips listKeys entirely
