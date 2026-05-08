5c0d0cbb564842cec4b0d8941e07b8e0f513d5ac
DRam76 <96955718+DRam76@users.noreply.github.com>
2026-05-08 21:38:11 +0100

    Add main.tf for Terraform configuration

---
+++ 07may2026-repos-terraform/infra/main.tf
@@ -0,0 +1,19 @@
+# main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
