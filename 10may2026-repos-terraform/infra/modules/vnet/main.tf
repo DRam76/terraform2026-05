ba5bc0119e29cb307e8d8916e461397a6d8b8b10
DRam76 <96955718+DRam76@users.noreply.github.com>
2026-05-09 22:41:11 +0100

    Add Azure Virtual Network and Subnet resources

---
+++ 10may2026-repos-terraform/infra/modules/vnet/main.tf
@@ -0,0 +1,44 @@
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "this" {
+  for_each = { for s in var.subnets : s.name => s }
+
+  name                 = each.value.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = each.value.address_prefixes
+
+  dynamic "delegation" {
+    for_each = lookup(each.value, "delegation", null) != null ? [each.value.delegation] : []
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name    = delegation.value.service_name
+        actions = delegation.value.actions
+      }
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "this" {
+  for_each = { for s in var.subnets : s.name => s if lookup(s, "create_nsg", false) }
+
+  name                = "nsg-${each.key}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  for_each = { for s in var.subnets : s.name => s if lookup(s, "create_nsg", false) }
+
+  subnet_id                 = azurerm_subnet.this[each.key].id
+  network_security_group_id = azurerm_network_security_group.this[each.key].id
+}
